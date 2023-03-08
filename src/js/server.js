b1dd2a3bcd7a8cc0ab31869f1f6637e57f33d2d3
brian <unasbrian@gmail.com>
2023-03-08 16:34:52 -0500

    fix. rollup json

--- src/js/server.js
+++ src/js/server.js
@@ -1,4 +1,4 @@
-//import axios from "axios"
+import axios from "axios"
 
 let conf
 let headers = {}
@@ -13,34 +13,34 @@ const ServerConnection = (() => {
     const wallet = {
         checkPreviewWithdrawal:(token)=>{
             var url=conf.API+`/checkRetailWithdrawal/${token}`;
-            //return axios.get(url,{headers});
+            return axios.get(url,{headers});
         },
         retailWithdrawal:(token, amount)=>{
             var url=conf.API+"/retailWithdrawal";
             var payload = {token, amount}
-            //return axios.post( url,JSON.stringify(payload),{headers} );
+            return axios.post( url,JSON.stringify(payload),{headers} );
 
         },
         depositRetail:(token, cod)=>{
             var url=conf.API+"/wallet/depositRetail";
             var payload = {token, cod}
-            //return axios.post( url,JSON.stringify(payload),{headers} );
+            return axios.post( url,JSON.stringify(payload),{headers} );
         },
         withdrawal_w :(token, amount, bank, account, info)=>{
             let payload = {token, amount, bank, account, info}
             let url = conf.API+"/withdrawal";
-            //return axios.post( url,JSON.stringify(payload),{headers} );
+            return axios.post( url,JSON.stringify(payload),{headers} );
         },
         bankDeposit:(token,bankDeposit )=>{
             let payload = {...bankDeposit, token}
             let url = conf.API+"/wallet/bankDeposit";
-            //return axios.post( url,JSON.stringify(payload),{headers} );
+            return axios.post( url,JSON.stringify(payload),{headers} );
         }
     }
     const user = {
         getBalance:(userToken)=>{
             let url = conf.API+`/balance/${userToken}`;
-            //return axios.get(url,{headers}) ;
+            return axios.get(url,{headers}) ;
         }
     }
     return {setConfig, wallet,user }
