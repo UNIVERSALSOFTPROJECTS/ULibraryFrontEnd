f9a71b1ec9b30928dab399b049d3ef1339cc37ff
Salvador Chumbe Cordova <salvdor290915@gmail.com>
2023-06-23 17:11:35 -0500

    Fix back to validate sms on register

--- src/UserRegisterStepbyStep.svelte
+++ src/UserRegisterStepbyStep.svelte
@@ -142,7 +142,7 @@
       ) {
         active_section = "user";
         return showNotify("error", "Este nombre de usuario ay existe");
-      } else if (response.data.errorCode == "SMS_CODE_INVALID" || response.data.message== "SMS invalid") {
+      } else if (data.errorCode == "SMS_CODE_INVALID") {
         active_section = "validateSMS";
         return showNotify("error", "Código SMS incorrecto");
       } else if (
